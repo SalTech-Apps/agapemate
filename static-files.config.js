910a80e46359a6e50ec7b0897af985447ef9b89b
copy-ninja1000 <emmajnr1000@gmail.com>
2025-01-14 10:01:30 +0100

    fix: braking path

--- static-files.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  staticDir: "./assets",
-  publicUrl: "./",
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+  ],
   theme: {
     theme: {
       fontFamily: {
-        'sans': ['ui-sans-serif', 'system-ui'],
-        'serif': ['ui-serif', 'Georgia'],
-        'mono': ['ui-monospace', 'SFMono-Regular'],
-        'display': ['"Itim" cursive', ],
-        'body': ['"Sniglet", system-ui'],
-      }
+        sans: ["ui-sans-serif", "system-ui"],
+        serif: ["ui-serif", "Georgia"],
+        mono: ["ui-monospace", "SFMono-Regular"],
+        display: ['"Itim" cursive'],
+        body: ['"Sniglet", system-ui'],
+      },
     },
     extend: {
       colors: {
         primary: "#F08100",
-        pink:"#FEF7F0",
-        "text-black":"#21201F"
+        pink: "#FEF7F0",
+        "text-black": "#21201F",
       },
-     
     },
   },
   plugins: [],
