7d2b71bafbcda8e0d65aa08625b2c3a6f703a0d6
copy-ninja1000 <emmajnr1000@gmail.com>
2025-01-17 20:22:53 +0100

    fix: Testimonies

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 import { toggleFAQ } from "./faq_collapse.js";
 import { toggleMobileMenu } from "./navbar.js";
+import { updateCarousel, setTestimonial } from "./testimonies.js";
 
 window.toggleFAQ = toggleFAQ;
 window.toggleMobileMenu = toggleMobileMenu;
+window.setTestimonial = setTestimonial;
+window.updateCarousel = updateCarousel;
